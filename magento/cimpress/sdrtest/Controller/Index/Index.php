6244d5d366a0fab6e3e7eedf04364fcbccb8eba5
Stephen Roylance <sdr@stephens-mbp.vistaprint.net>
2016-06-30 11:32:16 -0400

    hello world code

---
+++ magento/cimpress/sdrtest/Controller/Index/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace cimpress\sdrtest\Controller\Index;
+ 
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo 'Hello MCP!';
+    }
+}
