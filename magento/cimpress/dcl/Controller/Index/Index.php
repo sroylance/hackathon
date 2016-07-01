1a4e0c63e5644bd850cdbd3bd58fcd6ae06e90e3
Benjamin Clark <bennyc@gmail.com>
2016-07-01 09:44:27 -0400

    Further plumbing for DCL index.html

--- magento/cimpress/dcl/Controller/Index/Index.php
+++ magento/cimpress/dcl/Controller/Index/Index.php
@@ -1,10 +1,47 @@
 <?php
-namespace cimpress\dcl\Controller\Index;
- 
-class Index extends \Magento\Framework\App\Action\Action
+namespace cimpress\dcl\Controller\Adminhtml\Import;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface; // Needed to retrieve config values
+
+class Index extends \Magento\Backend\App\Action
 {
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        ScopeConfigInterface $scopeConfig // Needed to retrieve config values
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+        $this->scopeConfig = $scopeConfig; // Needed to retrieve config values
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
     public function execute()
     {
-        echo 'DCL CONTENT GOES HERE';
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        #$cfg_token = $this->scopeConfig->getValue('sdrtest/general/refreshtoken');
+
+
+        #$resultPage->getLayout()->getBlock('Test')->setStuff($this->APIClient->getVcsProducts());
+        #var_dump($this->APIClient->getVcsProducts());
+
+        return $resultPage;
     }
 }
